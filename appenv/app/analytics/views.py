19c01e0796407786e302490d3e6517e39fed7920
MD Dubey <mddubey409@gmail.com>
2018-01-27 11:04:47 +0530

    Dubey | Reading Bahmni URL, Username and password as enviornment variables.

--- appenv/app/analytics/views.py
+++ appenv/app/analytics/views.py
@@ -1,11 +1,13 @@
 import datetime
 import json
 import requests
+import os
 from django.http import JsonResponse
 from django.shortcuts import render
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+bahmni_base_url = os.environ.get('BAHMNI_BASE_URL')
 
 
 def index(request):
@@ -16,7 +18,7 @@ def index(request):
 
 
 def allReports(request):
-    url = 'https://192.168.33.20/bahmnireports/allReports'
+    url = '%s/bahmnireports/allReports' % bahmni_base_url
     response = requests.get(url, verify=False, cookies=(loginAndGetCookies()))
     return JsonResponse(json.loads(response.content), safe=False)
 
@@ -30,15 +32,17 @@ def reportData(request):
     report_params["startDate"] = f"{one_year_ago:%Y-%m-%d}"
     report_params["endDate"] = f"{today:%Y-%m-%d}"
 
-    url = 'https://192.168.33.20/bahmnireports/report'
+    url = '%s/bahmnireports/report' % bahmni_base_url
     response = requests.get(url, verify=False, cookies=(loginAndGetCookies()), params=report_params)
     print(response.content)
     return JsonResponse(json.loads(response.content), safe=False)
 
 
 def loginAndGetCookies():
-    credentials = {'uname': 'random', 'pw': 'aurrandom'}
-    login_url = "https://192.168.33.20/openmrs/ms/legacyui/loginServlet"
+    username = os.environ.get('BAHMNI_REPORTS_USERNAME')
+    password = os.environ.get('BAHMNI_REPORTS_PASSWORD')
+    credentials = {'uname': username, 'pw': password}
+    login_url = "%s/openmrs/ms/legacyui/loginServlet" % bahmni_base_url
     login_response = requests.post(login_url, verify=False, params=credentials)
     cookies = login_response.cookies
     return cookies
